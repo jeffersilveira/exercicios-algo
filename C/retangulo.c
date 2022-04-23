bd073deb4a57751611bc862e402e2835424c3ddc
jeffersilveira <jeffersonsilveira@live.com>
2022-04-23 11:18:53 -0300

    algoritmo com sqrt

---
+++ C/retangulo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double base, altura, area, perimetro, diagonal;
+
+    printf("Base do retangulo: ");
+    scanf("%lf", &base);
+
+    printf("Altura do retangulo: ");
+    scanf("%lf", &altura);
+
+    area = altura * base;
+    printf("AREA = %.4lf\n", area);
+
+    perimetro = 2 * (altura + base);
+    printf("PERIMETRO = %.4lf\n", perimetro);
+
+    diagonal = sqrt(base * base + altura * altura);
+    printf("DIAGONAL = %.4lf\n", diagonal);
+
+    return 0;
+}
